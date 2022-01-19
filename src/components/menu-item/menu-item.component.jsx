ab2dfbf7b741df057d05679c695a6db573b37f0b
jangradev <jangradev@gmail.com>
2022-01-19 17:28:57 +0530

    lect upto 82 _ withRouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  // eslint-disable-next-line no-restricted-globals
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className='background-image' style={{ background: `url(${imageUrl})` }}></div>
 
     <div className='content'>
@@ -11,4 +13,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
   </div>
 );
-export default MenuItem;
+export default withRouter(MenuItem);
